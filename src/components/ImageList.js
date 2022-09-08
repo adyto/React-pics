37a01aadb054c05898e2669f2485e16849300e40
ady.to <adiyulianto61@gmail.com>
2022-09-08 15:22:58 +0700

    refresh page

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,24 +1,13 @@
 import React from "react";
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-  const images = props.images.map(
-    ({ alt_description, description, id, urls }) => {
-      return (
-        <div key={id}>
-          <h2 style={{ textTransform: "capitalize", textAlign: "center" }}>
-            {alt_description}
-          </h2>
-          <img alt={description} src={urls.regular} />
-        </div>
-      );
-    },
-  );
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
+  });
 
-  return (
-    <div>
-      <h1>{images}</h1>
-    </div>
-  );
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
